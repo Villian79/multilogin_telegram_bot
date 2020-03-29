1be5b1c81fee8a17124fa8784a884e8883955ba8
Villian79 <ivilinchuk@gmail.com>
2020-03-29 19:33:00 +0300

    add command hanlers

--- application.py
+++ application.py
@@ -21,18 +21,63 @@ TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Howdy!!! How are you doing?", disable_notification=True)
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    start_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    start_markup.row('/start', '/help', '/hide')
 
+    bot.send_message(
+            message.chat.id, 
+            " 🤖 Мультилогин Бот готов к работе! \n\n⚙ Нажмите /help и увидите, что он может"
+    )
+
+    bot.send_message(
+            message.from_user.id, 
+            "⌨️ Кнопочное меню добавлено!\n⌨️ /hide и кнопочное меню будет закрыто", 
+            reply_markup=start_markup
+    )
+
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    help_markup = telebot.types.InlineKeyboardMarkup()
+    help_markup.row(
+        telebot.types.InlineKeyboardButton('Мультилогин - что это?', callback_data='get-about'), 
+        telebot.types.InlineKeyboardButton('Документация', callback_data='get-docs')
+    )
+    help_markup.row(
+        telebot.types.InlineKeyboardButton('Прайсинг', callback_data='get-price'), 
+        telebot.types.InlineKeyboardButton('Прокси рекомендации', url='https://multilogin.com/ru/proxy/')
+    )
+    help_markup.row(
+        telebot.types.InlineKeyboardButton('ЧаВо', callback_data='get-price'), 
+        telebot.types.InlineKeyboardButton('Свяжитесь с нами', callback_data='get-contact')
+    )
+
+
+    bot.send_message(
+        message.chat.id,
+        '✅ "Мультилогин - что это?" - Узнайте больше о приложении\n\n' +
+        '📚 "Документация" - Изучите наш функционал он-лайн\n\n' +
+        '💵 "Прайсинг" - Цены и тарифные пакеты\n\n' +
+        '🥇 "Прокси рекомендации" - Прокси, которые мы рекомендуем\n\n' +
+        '📌 "ЧаВо" - ответы на реальные вопросы от реальных людей\n\n'+
+        '📬 "Свяжитесь с нами" - просто кнопка для связи\n',
+        reply_markup=help_markup
+    )
+
+@bot.message_handler(commands=['hide'])
+def command_hide(message):
+    hide_markup = telebot.types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, "⌨💤...", reply_markup=hide_markup)
+
+  
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    print(message)
-	#bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 @bot.message_handler(content_types=['sticker'])
 def echo_any(message):
-    print(message.sticker)
+    print(message.sticker)  
 
 
 bot.polling(timeout=60)
